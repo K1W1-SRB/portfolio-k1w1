e39319b3c45fe306d219826d08dc9611979318ba
K1W1-SRB <sebastiansomogyi@gmail.com>
2022-11-18 20:17:34 +0000

    hopefully repo

---
+++ components/Hero.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import BackgroundCircles from "./BackgroundCircles";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import Link from "next/link";
+
+type Props = {};
+
+export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, its Sebastian Somogyi",
+      "Guy-who-loves-Coffee.tsx",
+      "<Software Engineer K1W1 />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div className="h-screen scroll-smooth flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <img
+        src="k1w1 logo illustator.svg"
+        className=" relative rounded-fill h-32 mx-auto object-cover"
+      />
+      <div className="z-20 ">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#f5deb3" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#WorkExperience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#Skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#Projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
